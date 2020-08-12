df0ac73fd2a1a3657f1d2bbcef636567e2c38a8e
kantegory <davidobryakov@gmail.com>
2020-08-12 21:49:18 +0300

    Lint all files according to .editorconfig

--- utils/mongoConfig.js
+++ utils/mongoConfig.js
@@ -3,11 +3,11 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'open-teuxdeux';
 
 class MongoConfig {
-	constructor(url, dbName, MongoClient) {
-		this.url = url;
-		this.dbName = dbName;
-		this.MongoClient = MongoClient;
-	}
+  constructor(url, dbName, MongoClient) {
+    this.url = url;
+    this.dbName = dbName;
+    this.MongoClient = MongoClient;
+  }
 }
 
 const Config = new MongoConfig(url, dbName, MongoClient);
